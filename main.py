c9f49844ad8a4bf582c74c10a564fe381645028f
Aidan <aidan@digitgaming.com>
2017-01-09 03:36:51 +0000

    connect to mysql db

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import MySQLdb
+
 from flask import Flask
 
 from scoreboard.handler import ScoreBoardHandler
 
-app = Flask(__name__)
 
-ScoreBoardHandler().register(app)
+def initialise_database():
+    db=MySQLdb.connect(user="root", passwd="panda123")
+    ScoreBoardHandler().attach_db(db)
+
+
+def initialise_handlers(app):
+    ScoreBoardHandler().register(app)
 
 if __name__ == "__main__":
+    app = Flask(__name__)
+
+    initialise_database()
+    initialise_handlers(app)
+
     app.run()
--- scoreboard/handler.py
+++ scoreboard/handler.py
@@ -5,5 +5,9 @@ class ScoreBoardHandler(Handler):
     def ping(self):
         return self.package_response("pong")
 
+    def attach_db(self, db):
+        self.db = db
+
     def __init__(self):
+        self.db = None
         self.services = {"/": self.ping}
