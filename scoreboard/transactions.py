44a6925be9dbb34df518860b24ca4c7c1a3f8da5
Aidan <aidan@digitgaming.com>
2017-01-11 07:29:21 +0000

    validation step

--- scoreboard/transactions.py
+++ scoreboard/transactions.py
@@ -1,2 +1,7 @@
+from database.storagelayer import execute_mysql
+
+from scoreboard.controller import validate_table_create
+
 def create_table_transaction(table_name, column_names, column_data_types):
-    pass
+    validate_table_create(table_name, column_data_types, column_data_types)
+    
