9830ad1c7f89fc296fd7a223875c1334d1642d16
Aidan <aidan@digitgaming.com>
2017-01-11 08:12:45 +0000

    parsing args

--- scoreboard/transactions.py
+++ scoreboard/transactions.py
@@ -4,4 +4,7 @@ from scoreboard.controller import validate_table_create
 
 def create_table_transaction(table_name, column_names, column_data_types):
     validate_table_create(table_name, column_data_types, column_data_types)
-    
+    args = [item for pair in zip(column_names, column_data_types) for item in pair]
+    args = " ".join(args)
+    command = "CREATE TABLE {}({})".format(table_name, args)
+    execute_mysql(command)
