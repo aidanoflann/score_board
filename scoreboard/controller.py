cf525e2f2a2534b749d28d97ba80b0aec308a183
Aidan <aidan@digitgaming.com>
2017-01-12 14:09:16 +0000

    remove needless validation step

--- scoreboard/controller.py
+++ scoreboard/controller.py
@@ -1,6 +1,4 @@
 def validate_table_create(table_name, column_names, column_data_types):
-    if not isinstance(table_name, str):
-        raise ValueError("Cannot create table. Table name must be a string: '{}'".format(table_name))
     if len(column_names) != len(column_data_types):
         raise ValueError("Cannot create table. {} column names but {} column data types were given."
                          .format(len(column_names), len(column_data_types)))
