2b80ef50d3030515538440fa22759f62902d8bf5
Aidan <aidan@digitgaming.com>
2017-01-14 21:00:35 +0000

    added create scoreboard t+ep

--- scoreboard/handler.py
+++ scoreboard/handler.py
@@ -2,10 +2,16 @@ from flask import request
 
 from core import Handler
 
-from scoreboard.transactions import insert_score_transaction
+from scoreboard.transactions import insert_score_transaction, create_scoreboard_transaction
 
 
 class ScoreBoardHandler(Handler):
+    def create_scoreboard(self):
+        request_json = request.get_json(force=True)
+        game_name = request_json.get("game_name")
+        create_scoreboard_transaction(game_name)
+        return "Done"
+
     def insert_score(self):
         request_json = request.get_json(force=True)
         game_name = request_json.get("game_name")
@@ -17,5 +23,6 @@ class ScoreBoardHandler(Handler):
     def __init__(self):
         self.db = None
         self.services = {
-            "/insert_score": self.insert_score
+            "/insert_score": self.insert_score,
+            "/create_scoreboard", self.create_scoreboard
         }
--- scoreboard/transactions.py
+++ scoreboard/transactions.py
@@ -1,4 +1,14 @@
-from sql_wrapper.transactions import insert_into_table_transaction
+from sql_wrapper.transactions import insert_into_table_transaction, create_table_transaction
+
+
+def create_scoreboard_transaction(game_name):
+    """ Create a fresh scoreboard for a game.
+
+    :param game_name:   name of the game. used as the scoreboard name.
+    """
+    column_names = ["user", "score"]
+    column_data_types = ["TEXT", "INTEGER"]
+    create_table_transaction(game_name, column_names, column_data_types)
 
 
 def insert_score_transaction(game_name, user, score):
@@ -7,7 +17,6 @@ def insert_score_transaction(game_name, user, score):
     :param game_name:
     :param user:
     :param score:
-    :return:
     """
     field_names = ["user", "score"]
     field_values = [user, score]
