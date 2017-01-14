28a6750c1536836dc2707413e6ea19723abed8b6
Aidan <aidan@digitgaming.com>
2017-01-14 09:22:52 +0000

    parse create table inputs properly:

--- database/storagelayer.py
+++ database/storagelayer.py
@@ -4,11 +4,12 @@ db = None
 
 
 def connect():
-    db = MySQLdb.connect(user="root", passwd="panda123")
+    db = MySQLdb.connect(user="root", passwd="panda123", db="test")
     return db
 
 
 def execute_mysql(command):
     cursor = connect().cursor()
+    # TODO: get cursor without reconnecting?
     cursor.execute(command)
     cursor.close()
--- scoreboard/transactions.py
+++ scoreboard/transactions.py
@@ -5,7 +5,9 @@ from scoreboard.controller import validate_table_create
 
 def create_table_transaction(table_name, column_names, column_data_types):
     validate_table_create(table_name, column_data_types, column_data_types)
-    args = [item for pair in zip(column_names, column_data_types) for item in pair]
+    # generate comma-separated inputs to CREATE TABLE from the two input lists
+    # TODO: make this next line not horrible
+    args = [item for pair in zip(column_names, column_data_types, [","] * len(column_names)) for item in pair][:-1]
     args = " ".join(args)
     command = "CREATE TABLE {}({})".format(table_name, args)
     execute_mysql(command)
