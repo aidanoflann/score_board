a5615fa669e50462250b3f0fd31f559e7ee512f1
Aidan <aidanoflann@gmail.com>
2017-06-24 22:13:16 +0100

    simplify connection a bit - can re-add later

--- src/database/storagelayer.py
+++ src/database/storagelayer.py
@@ -1,33 +1,13 @@
-import time
-
 import MySQLdb
-
-import settings
-
-db = None
 
 
 def connect():
-    # Create the database if not present
-    # TODO: put all inside if db doesn't exist block (no need to connect twice every time)
-    attempts = 0
+    # TODO: Create the database if not present
     print("Attempting to connect to mysql database...")
-    while attempts < settings.RETRY_ATTEMPTS:
-        try:
-            db = MySQLdb.connect(host="mysql_db", user=settings.MYSQL_USERNAME, passwd=settings.MYSQL_PASSWORD)
-            cursor = db.cursor()
-            sql = 'CREATE DATABASE IF NOT EXISTS {}'.format(settings.MYSQL_SCOREBOARD_DB_NAME)
-            cursor.execute(sql)
-
-            db = MySQLdb.connect(user=settings.MYSQL_USERNAME, passwd=settings.MYSQL_PASSWORD,
-                                 db=settings.MYSQL_SCOREBOARD_DB_NAME)
-        except Exception as e:
-            print("Attempt to connect to mysql DB failed. Retrying in {} seconds.".format(settings.RETRY_PERIOD))
-            print("Attempt number: {}. Max attempts: {}.".format(attempts, settings.RETRY_ATTEMPTS))
-            print("Error args: {}".format(str(e.args)))
-            attempts += 1
-            time.sleep(settings.RETRY_PERIOD)
-
+    db = MySQLdb.connect(host="mysql_db", user="root", passwd="my-secret-pw", db="test")
+    if db is None:
+        raise Exception("Failed to connect to mysql database. Exiting...")
+    print("Connection successful.")
     return db
 
 
