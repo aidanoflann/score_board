5a0f61cd3ac4c5f021bfec20c4180dca6b20f756
Aidan <aidanoflann@gmail.com>
2017-06-25 00:44:28 +0100

    but the links though

--- src/database/storagelayer.py
+++ src/database/storagelayer.py
@@ -7,11 +7,11 @@ def connect():
     # Create the database if not present
     # TODO: put all inside if db doesn't exist block (no need to connect twice every time)
     print("Attempting to connect to mysql database...")
-    db = MySQLdb.connect(host="score_board_and_db", user=settings.MYSQL_USERNAME, passwd=settings.MYSQL_PASSWORD)
+    db = MySQLdb.connect(host="mysql_db", user=settings.MYSQL_USERNAME, passwd=settings.MYSQL_PASSWORD)
     cursor = db.cursor()
     sql = 'CREATE DATABASE IF NOT EXISTS {}'.format(settings.MYSQL_SCOREBOARD_DB_NAME)
     cursor.execute(sql)
-    db = MySQLdb.connect(host="score_board_and_db", user="root", passwd="my-secret-pw", db="test")
+    db = MySQLdb.connect(host="mysql_db", user="root", passwd="my-secret-pw", db="test")
     if db is None:
         raise Exception("Failed to connect to mysql database. Exiting...")
     print("Connection successful.")
