8e445fff728476cf7a822edfaeb5c21799d590ec
Aidan <aidanoflann@gmail.com>
2017-06-24 13:41:31 +0100

    reduce retry period

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ MYSQL_USERNAME = os.getenv("APP_MYSQL_USERNAME", "root")
 MYSQL_PASSWORD = os.getenv("APP_MYSQL_PASSWORD", "my-secret-pw")
 MYSQL_SCOREBOARD_DB_NAME = os.getenv("APP_MYSQL_SCOREBOARD_DB_NAME", "test")
 
-RETRY_PERIOD = 10  # seconds
+RETRY_PERIOD = 1  # seconds
 RETRY_ATTEMPTS = 10
