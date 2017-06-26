91fb0233177042cf0b8fd489aefd1b4a0999eaed
Aidan <aidanoflann@gmail.com>
2017-06-26 23:33:11 +0100

    set up API key

--- src/core.py
+++ src/core.py
@@ -1,3 +1,9 @@
+from flask import request
+
+from functools import wraps
+
+import settings
+
 class Handler():
     def __init__(self):
         self.services = {}  # format is {path: callable}
@@ -5,8 +11,20 @@ class Handler():
     def register(self, app):
         for path, func in self.services.iteritems():
             if func.__code__.co_argcount == 1:
-                app.route(path, methods=["POST"])(func)
+                app.route(path, methods=["POST"])(check_header(func))
 
     @staticmethod
     def package_response(response):
         return str(response)
+
+
+def check_header(func):
+    @wraps(func)
+    def func_wrapper():
+        api_key = request.headers.get("x-api-key", None)
+        if api_key is None:
+            raise ValueError("API Key not found")
+        if api_key != settings.API_KEY:
+            raise ValueError("API Key invalid")
+        return func()
+    return func_wrapper
--- src/settings.py
+++ src/settings.py
@@ -8,6 +8,7 @@ MYSQL_DB_URL = os.getenv("APP_MYSQL_DB_URL", "mysql_db")
 STARTUP_WAIT = 20
 RETRY_PERIOD = 1  # seconds
 RETRY_ATTEMPTS = 3
+API_KEY = "iamverysmartandsocameupwithaverycleverandlongpassword"
 
 try:
     from local_settings import *
